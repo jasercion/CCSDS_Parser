b0622b545db69f0fe8f1202b0b0681b51dd7908b
Joe A <jasercion.gwu@gmail.com>
2019-05-02 23:27:54 +0000

    warning cleanup and fixes

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,19 @@ extern crate ccsds_primary_header;
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
-use std::io::Error;
 use std::io::BufReader;
 use bytes::Bytes;
 use ccsds_primary_header::primary_header::*;
 use ccsds_primary_header::parser::*;
 
-/// # parseInput() function
-/// `parseInput()` should accept an object representing a
+/// # parse_input() function
+/// `parse_input()` should accept an object representing a
 /// loaded CCSDS compliant data file, create an instance of
 /// a CCSDS Header Parser, process the data, and return the
 /// header to main in a useful data format
 ///
 
-fn parseInput(bytestream: Bytes) {
+fn parse_input(bytestream: Bytes) {
     let mut parser = ccsds_primary_header::parser::CcsdsParser::new();      
     parser.recv_bytes(bytestream);
 }
@@ -31,7 +30,7 @@ fn parseInput(bytestream: Bytes) {
 ///
 
 fn main() -> Result<(), std::io::Error> {
-    let args: Vec::<String> = env::args().collect();
+    let args: Vec<String> = env::args().collect();
     println!("Loading data files {:?}...", args);
 
     let file = match File::open(&args[0]) {
@@ -48,8 +47,8 @@ fn main() -> Result<(), std::io::Error> {
 
     reader.read_to_end(&mut buffer);
 
-    let mut mem = Bytes::from(buffer);
-    parseInput(mem);
+    let mem = Bytes::from(buffer);
+    parse_input(mem);
 
     println!("Program terminated sucessfully!");
     Ok(())
